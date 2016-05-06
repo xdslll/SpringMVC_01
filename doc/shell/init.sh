3053bac49df76d98bfd00eaa061c2bc20bc8df52
sam <xdslll@163.com>
2016-05-06 09:15:55 +0800

    更新系统相关配置文件

---
+++ doc/shell/init.sh
@@ -0,0 +1,8 @@
+mkdir /Users/
+mkdir /Users/apple/
+mkdir /Users/apple/Desktop/
+mkdir /Users/apple/Desktop/upload/
+mkdir /Users/apple/Desktop/upload/category/
+mkdir /Users/apple/Desktop/upload/export/
+mkdir /Users/apple/Desktop/upload/licence/
+mkdir /Users/apple/Desktop/upload/temp/
--- doc/sql/v1_1.sql
+++ doc/sql/v1_1.sql
@@ -1,4 +1,3 @@
---创建区域表
 CREATE TABLE `enford_product_area` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) NOT NULL,
@@ -6,14 +5,11 @@ CREATE TABLE `enford_product_area` (
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='区域表';
 
---修改部门表
-ALTER TABLE `test`.`enford_product_department` 
+ALTER TABLE `sg`.`enford_product_department`
 ADD COLUMN `area_id` INT NULL AFTER `comp_id`;
 
---设置部门的默认区域为-1
-ALTER TABLE `test`.`enford_product_department`
+ALTER TABLE `sg`.`enford_product_department`
 CHANGE COLUMN `area_id` `area_id` INT(11) NULL DEFAULT -1;
 
---将现有的部门区域id刷为-1
 set SQL_SAFE_UPDATES=0;
 update enford_product_department set area_id=-1 where area_id is null;
